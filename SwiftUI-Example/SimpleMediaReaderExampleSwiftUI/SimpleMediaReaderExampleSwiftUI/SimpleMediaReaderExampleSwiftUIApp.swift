7f0916830647e9b792c40ce96b14c2f31683458c
Fumiya Sakai <just1factory@gmail.com>
2021-10-02 10:44:30 +0900

    UIKit/SwiftUIのプロジェクトを追加する

---
+++ SwiftUI-Example/SimpleMediaReaderExampleSwiftUI/SimpleMediaReaderExampleSwiftUI/SimpleMediaReaderExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMediaReaderExampleSwiftUIApp.swift
+//  SimpleMediaReaderExampleSwiftUI
+//
+//  Created by 酒井文也 on 2021/10/02.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMediaReaderExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UIKit-Example/SimpleMediaReaderExampleUIKit/SimpleMediaReaderExampleUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMediaReaderExampleUIKit
+//
+//  Created by 酒井文也 on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
