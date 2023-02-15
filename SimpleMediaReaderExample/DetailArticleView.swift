4362b5df047c50b5762f43b828df644badcc0dea
fumiyasac <just1factory@gmail.com>
2023-02-15 09:41:12 +0900

    body要素の追記をする

--- SimpleMediaReaderExample/DetailArticleView.swift
+++ SimpleMediaReaderExample/DetailArticleView.swift
@@ -11,7 +11,12 @@ import SwiftUI
 
 struct DetailArticleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        Group {
+            ScrollView {
+                Text("DetailArticleViewから移植する")
+            }
+        }
     }
 }
 
