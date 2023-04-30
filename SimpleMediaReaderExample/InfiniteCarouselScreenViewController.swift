f0ce6ee5b29c24a2ae5c6c94bc63772b41b71ad9
fumiyasac <just1factory@gmail.com>
2023-04-30 17:03:42 +0900

    InfiniteCarouselScreenの画面要素を追加する対応

---
+++ SimpleMediaReaderExample/InfiniteCarouselScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfiniteCarouselScreenViewController.swift
+//  SimpleMediaReaderExample
+//
+//  Created by 酒井文也 on 2023/04/30.
+//
+
+import UIKit
+
+class InfiniteCarouselScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
