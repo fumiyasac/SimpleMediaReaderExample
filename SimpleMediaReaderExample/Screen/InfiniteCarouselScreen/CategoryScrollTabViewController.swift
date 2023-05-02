b1a37ea1deb5d61e391379e06d678016de24e905
fumiyasac <just1factory@gmail.com>
2023-05-02 19:50:12 +0900

    CategoryScrollTabViewCellの追加とGroup整理

---
+++ SimpleMediaReaderExample/Screen/InfiniteCarouselScreen/CategoryScrollTabViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryScrollTabViewController.swift
+//  SimpleMediaReaderExample
+//
+//  Created by 酒井文也 on 2023/05/02.
+//
+
+import UIKit
+
+final class CategoryScrollTabViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ SimpleMediaReaderExample/Screen/InfiniteCarouselScreen/ViewParts/CategoryScrollTabViewCell/CategoryScrollTabViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryScrollTabViewCell.swift
+//  SimpleMediaReaderExample
+//
+//  Created by 酒井文也 on 2023/05/02.
+//
+
+import UIKit
+
+final class CategoryScrollTabViewCell: UICollectionViewCell {
+
+    // MARK: - Override
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
