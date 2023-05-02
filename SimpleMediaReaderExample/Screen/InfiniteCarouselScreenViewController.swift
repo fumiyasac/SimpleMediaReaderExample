c9048aa33c28ce9e74ad294a95b3a20c07bc5dc9
fumiyasac <just1factory@gmail.com>
2023-05-02 19:41:15 +0900

    Groupの整理

---
+++ SimpleMediaReaderExample/Screen/InfiniteCarouselScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteCarouselScreenViewController.swift
+//  SimpleMediaReaderExample
+//
+//  Created by 酒井文也 on 2023/04/30.
+//
+
+import UIKit
+
+final class InfiniteCarouselScreenViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
