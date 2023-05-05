da4cb1c9e96f1321f380932f006668d59eeb0419
fumiyasac <just1factory@gmail.com>
2023-05-05 21:14:32 +0900

    他のScreenViewの追加

---
+++ SimpleMediaReaderExample/Screen/ProfileScreen/ProfilScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfilScreenViewController.swift
+//  SimpleMediaReaderExample
+//
+//  Created by 酒井文也 on 2023/05/05.
+//
+
+import UIKit
+
+final class ProfilScreenViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
