322b8b0d375dbadd5c6684c7eefad8a6bd2fa342
fumiyasac <just1factory@gmail.com>
2023-06-16 22:45:15 +0900

    Redux・TCA比較用のプロジェクトを2つ準備した

---
+++ SimpleMediaReaderExampleTCA/SimpleMediaReaderExampleTCA/SimpleMediaReaderExampleTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMediaReaderExampleTCAApp.swift
+//  SimpleMediaReaderExampleTCA
+//
+//  Created by 酒井文也 on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMediaReaderExampleTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
