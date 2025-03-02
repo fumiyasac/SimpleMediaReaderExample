a9064677473147a0681f6764ecc04b91c2a82eb8
fumiyasac <just1factory@gmail.com>
2025-03-02 11:04:51 +0900

    微調整を加える対応

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,18 +1,22 @@
+//
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 
+//
 const prisma = new PrismaClient();
 const app = express();
+
+//
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 
+//
 app.get('/', async (req, res) => {
   const foods = await prisma.food.findMany();
   res.json(foods);
 })
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+//
+app.listen(3000, () => {
+  console.log(`SimpleMediaReaderExample Mock Server is running...`);
 });
