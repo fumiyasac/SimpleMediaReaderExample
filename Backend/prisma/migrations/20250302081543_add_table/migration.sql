2d2f74faabfc198eead59d5c82aa81ebe00faa1f
fumiyasac <just1factory@gmail.com>
2025-03-02 17:16:32 +0900

    新たなテーブル定義を追加する

---
+++ Backend/prisma/migrations/20250302081543_add_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Pickup` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `summary` VARCHAR(191) NOT NULL,
+    `thumbnailUrl` VARCHAR(191) NOT NULL,
+    `publishedAt` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
