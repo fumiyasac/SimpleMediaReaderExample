a5e93219e96e2e3c6d9c011810a8cc951c12ec02
fumiyasac <just1factory@gmail.com>
2025-03-07 20:21:35 +0900

    DBのMigrationを再度実行する

---
+++ Backend/prisma/migrations/20250307111959_add_tables/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `Food` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NULL,
+    `category` VARCHAR(191) NULL,
+    `summary` VARCHAR(191) NULL,
+    `thumbnailUrl` VARCHAR(191) NULL,
+    `publishedAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Pickup` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NULL,
+    `summary` VARCHAR(191) NULL,
+    `thumbnailUrl` VARCHAR(191) NULL,
+    `publishedAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
