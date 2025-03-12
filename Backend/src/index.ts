b26ac2346bb7c158f32b1f62afb97b13950f26fa
fumiyasac <just1factory@gmail.com>
2025-03-12 18:39:50 +0900

    GETで取得するEndpointを仮に設定

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -7,10 +7,18 @@ const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.get('/', async (request, response) => {
-  const foods = await prisma.food.findMany();
+server.get('/food', async (request, response) => {
+  const page = parseInt(request.url.split("?page=")[1]);
+  const take = 10;
+  const skip = (page - 1) * take;
+  const foods = await prisma.food.findMany({ skip: skip, take: take });
   response.json(foods);
-})
+});
+
+server.get('/pickup', async (_, response) => {
+  const pickup = await prisma.food.findMany();
+  response.json(pickup);
+});
 
 server.listen(3000, () => {
   console.log(`SimpleMediaReaderExample Mock Server is running...`);
