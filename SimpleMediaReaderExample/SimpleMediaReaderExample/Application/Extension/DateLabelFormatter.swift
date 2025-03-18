c11dc763e7fb478a40b23457c3f44d6ad81d329d
fumiyasac <just1factory@gmail.com>
2025-03-18 17:06:47 +0900

    View要素の追加と日付表示対応

---
+++ SimpleMediaReaderExample/SimpleMediaReaderExample/Application/Extension/DateLabelFormatter.swift
@@ -0,0 +1,39 @@
+//
+//  DateLabelFormatter.swift
+//  SimpleMediaReaderExample
+//
+//  Created by 酒井文也 on 2025/03/18.
+//
+
+import Foundation
+
+final class DateLabelFormatter {
+
+    // MARK: - Properties
+
+    private static let convertDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        return dateFormatter
+    }()
+
+    private static let stringDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = TimeZone.current
+        return dateFormatter
+    }()
+
+    // MARK: - Static Functions
+
+    // APIで取得された日付フォーマットを任意の表記に変換する
+    static func getDateStringFromAPI(apiDateString: String, printFormatter: String = "yyyy.MM.dd") -> String {
+        guard let apiDate = convertDateFormatter.date(from: apiDateString) else {
+            return "-"
+        }
+        stringDateFormatter.dateFormat = printFormatter
+        return stringDateFormatter.string(from: apiDate)
+    }
+}
--- SimpleMediaReaderExample/SimpleMediaReaderExample/Application/Screen/Food/FoodRowView.swift
+++ SimpleMediaReaderExample/SimpleMediaReaderExample/Application/Screen/Food/FoodRowView.swift
@@ -5,6 +5,7 @@
 //  Created by 酒井文也 on 2025/03/17.
 //
 
+import NukeUI
 import SwiftUI
 
 struct FoodRowView: View {
@@ -23,12 +24,68 @@ struct FoodRowView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0.0) {
-            HStack(spacing: 0.0) {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                VStack(alignment: .leading) {
-                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            HStack(alignment: .top, spacing: 0.0) {
+                LazyImage(url: URL(string: foodViewObject.thumbnailUrl)) { imageState in
+                    if let image = imageState.image {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 96.0, height: 64.0)
+                            .cornerRadius(8.0)
+                    } else {
+                        Color(uiColor: UIColor(code: "#dddddd"))
+                    }
                 }
+                VStack(alignment: .leading, spacing: 0.0) {
+                    Text(foodViewObject.title)
+                        .font(.callout)
+                        .fontWeight(.bold)
+                    VStack {
+                        Text(foodViewObject.category)
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.white)
+                            .padding(.horizontal, 8.0)
+                            .padding(.vertical, 2.0)
+                    }
+                    .background(.orange)
+                    // MEMO: 角丸にしたい場合には.cornerRadiusと.overlayを両方設定する必要がある
+                    .cornerRadius(16.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16.0)
+                            .stroke(.white, lineWidth: 1.0)
+                    )
+                    .padding(.vertical, 6.0)
+                    Text(DateLabelFormatter.getDateStringFromAPI(apiDateString: foodViewObject.publishedAt))
+                        .font(.footnote)
+                }
+                .padding(.horizontal, 8.0)
             }
+            .padding(.horizontal, 8.0)
+            HStack(spacing: 0.0) {
+                Text(foodViewObject.summary)
+                    .font(.callout)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+                Spacer()
+                Button(action: {
+                    
+                }, label: {
+                    if foodViewObject.isFavorited {
+                        Image(systemName: "star.fill")
+                    } else {
+                        Image(systemName: "star")
+                    }
+                })
+                .foregroundStyle(.yellow)
+                .buttonStyle(PlainButtonStyle())
+                .frame(width: 36.0, height: 36.0)
+            }
+            .padding(.horizontal, 8.0)
+            .padding(.vertical, 8.0)
+            Divider()
+                .background(.gray)
+                .padding(.horizontal, 8.0)
         }
     }
 }
@@ -43,7 +100,7 @@ struct FoodRowView: View {
         summary: "グルメ写真アプリサンプルNo.1になります。美味しそうな写真と一緒に是非雰囲気を味わってみて下さい😋",
         thumbnailUrl: "https://media-reader-example.s3.ap-northeast-1.amazonaws.com/meals_image_example1.jpg",
         publishedAt: "2025-03-10 00:00:00.000",
-        isFavorited: true
+        isFavorited: false
     )
     FoodRowView(foodViewObject: foodViewObject)
 }
