94fd15ff5bbe9fc331ff6e3b09e6b7ec5075fa43
fumiyasac <just1factory@gmail.com>
2025-03-29 21:56:42 +0900

    Pickupのセル要素を追加する

---
+++ SimpleMediaReaderExample/SimpleMediaReaderExample/Application/Screen/Pickup/PickupGridView.swift
@@ -0,0 +1,18 @@
+//
+//  PickupGridView.swift
+//  SimpleMediaReaderExample
+//
+//  Created by 酒井文也 on 2025/03/29.
+//
+
+import SwiftUI
+
+struct PickupGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickupGridView()
+}
