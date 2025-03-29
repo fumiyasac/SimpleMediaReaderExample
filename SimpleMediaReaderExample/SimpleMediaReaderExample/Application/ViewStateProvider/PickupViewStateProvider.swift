ea41d3b17e0462b351376b1fcb077142486b1df8
fumiyasac <just1factory@gmail.com>
2025-03-29 11:30:09 +0900

    PickupViewStateProviderを新規追加する

---
+++ SimpleMediaReaderExample/SimpleMediaReaderExample/Application/ViewStateProvider/PickupViewStateProvider.swift
@@ -0,0 +1,12 @@
+//
+//  PickupViewStateProvider.swift
+//  SimpleMediaReaderExample
+//
+//  Created by 酒井文也 on 2025/03/29.
+//
+
+import Foundation
+import Observation
+
+@Observable
+final class PickupViewStateProvider {}
