cce565b8ccad956d90c93a6c961bd399cfdcf0f5
fumiyasac <just1factory@gmail.com>
2025-03-29 21:56:54 +0900

    Pickupのセル要素を追加する

--- SimpleMediaReaderExample/SimpleMediaReaderExample/Application/Screen/Pickup/PickupGridView.swift
+++ SimpleMediaReaderExample/SimpleMediaReaderExample/Application/Screen/Pickup/PickupGridView.swift
@@ -5,14 +5,105 @@
 //  Created by 酒井文也 on 2025/03/29.
 //
 
+import NukeUI
 import SwiftUI
 
 struct PickupGridView: View {
+
+    // MARK: - Typealias
+
+    typealias TapGridViewAction = () -> Void
+
+    // MARK: - Property
+
+    private var tapGridViewAction: PickupGridView.TapGridViewAction
+    
+    private let pickupViewObject: PickupViewObject
+
+    // MARK: - Initializer
+
+    init(
+        pickupViewObject: PickupViewObject,
+        tapGridViewAction: @escaping PickupGridView.TapGridViewAction
+    ) {
+        self.pickupViewObject = pickupViewObject
+        self.tapGridViewAction = tapGridViewAction
+    }
+
+    // MARK: - Body
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            let gridWidth = CGFloat((proxy.size.width - 24.0) / 2)
+            VStack(alignment: .leading, spacing: 0.0) {
+                ZStack {
+                    LazyImage(url: URL(string: pickupViewObject.thumbnailUrl)) { imageState in
+                        if let image = imageState.image {
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: gridWidth, height: gridWidth)
+                                .cornerRadius(8.0)
+                        } else {
+                            Color(uiColor: UIColor(code: "#dddddd"))
+                                .frame(width: gridWidth, height: gridWidth)
+                                .cornerRadius(8.0)
+                        }
+                    }
+                    Rectangle()
+                        .foregroundColor(Color.black.opacity(0.36))
+                        .frame(width: gridWidth, height: gridWidth)
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Text(pickupViewObject.title)
+                                .font(.callout)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .padding(.horizontal, 8.0)
+                                .padding(.bottom, 10.0)
+                                .lineLimit(2)
+                            Spacer()
+                        }
+                    }
+                    .frame(width: gridWidth, height: gridWidth)
+                }
+                // (2) 下部のテキスト表示
+                VStack {
+                    Text(pickupViewObject.summary)
+                        .font(.callout)
+                        .foregroundColor(.primary)
+                        .padding(.horizontal, 8.0)
+                        .padding(.bottom, 2.0)
+                        .padding(.top, 8.0)
+                        .lineSpacing(2.0)
+                        .lineLimit(4)
+                }
+                // (3) 上に要素を詰めるためのSpacer
+                Spacer()
+            }
+            // MEMO: タップ領域の確保とタップ時の処理
+            .contentShape(Rectangle())
+            .onTapGesture(perform: tapGridViewAction)
+            .cornerRadius(8.0)
+            .frame(width: gridWidth, height: gridWidth + 102.0)
+            .background(
+                RoundedRectangle(cornerRadius: 8.0)
+                    .stroke(Color.secondary.opacity(0.5))
+            )
+        }
     }
 }
 
 #Preview {
-    PickupGridView()
+    PickupGridView(
+        pickupViewObject: PickupViewObject(
+            id: 1,
+            title: "ステーキの美味しいお店特集No.1🥩",
+            summary: "ジューシーな絶品ステーキを堪能できる人気店を厳選。肉好き必見の注目情報が満載！香り高いソースや多彩な部位も徹底紹介！",
+            thumbnailUrl: "https://media-reader-example.s3.ap-northeast-1.amazonaws.com/steak_image_example1.jpg",
+            publishedAt: "2025-03-10T00:00:00.000Z"
+        ),
+        tapGridViewAction: {}
+    )
 }
