f7e8e550f2380d22b5b1b96bf4f9100148c4b9a2
fumiyasac <just1factory@gmail.com>
2025-03-29 23:28:35 +0900

    Grid表示を整える

--- SimpleMediaReaderExample/SimpleMediaReaderExample/Application/Screen/Pickup/PickupScreen.swift
+++ SimpleMediaReaderExample/SimpleMediaReaderExample/Application/Screen/Pickup/PickupScreen.swift
@@ -9,19 +9,67 @@ import SwiftUI
 
 struct PickupScreen: View {
 
+    // MARK: - ViewStateProvider
+
+    private let pickupViewStateProvider: PickupViewStateProvider
+
+    // MARK: - Initializer
+
+    init(pickupViewStateProvider: PickupViewStateProvider = PickupViewStateProvider()) {
+        self.pickupViewStateProvider = pickupViewStateProvider
+    }
+
+    // MARK: - Property
+
+    private let gridColumns = [
+        GridItem(spacing: 8.0),
+        GridItem(spacing: 0.0)
+    ]
+    
     // MARK: - Body
 
     var body: some View {
         NavigationStack {
             Group {
-                Text("PickupScreen")
+                switch (pickupViewStateProvider.isLoading, pickupViewStateProvider.errorMessage) {
+                case (true, _):
+                    // Loading Indicatorを表示する
+                    ExecutingConnectionView()
+                case (_, pickupViewStateProvider.errorMessage) where pickupViewStateProvider.errorMessage != nil:
+                    // Error Message画面を表示する
+                    ConnectionErrorView(
+                        tapButtonAction: {
+                            pickupViewStateProvider.fetchInitialPickups()
+                        }
+                    )
+                default:
+                    GeometryReader { proxy in
+                        let gridWidth = CGFloat((proxy.frame(in: .global).size.width - 24.0) / 2)
+                        ScrollView {
+                            // MEMO: 上下に8.0の行間をつけるためにSpacing値を8.0としています。
+                            LazyVGrid(columns: gridColumns, spacing: 8.0) {
+                                ForEach(pickupViewStateProvider.pickupViewObjects) { pickupViewObject in
+                                    PickupGridView(pickupViewObject: pickupViewObject, tapGridViewAction: {
+                                        print("Tapped")
+                                    })
+                                    .frame(width: gridWidth, height: gridWidth + 102.0)
+                                }
+                            }
+                            .padding(8.0)
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                }
             }
             .navigationBarTitle("🍒Pickup")
             .navigationBarTitleDisplayMode(.inline)
+            .onFirstAppear {
+                pickupViewStateProvider.fetchInitialPickups()
+            }
         }
     }
 }
 
-#Preview {
-    PickupScreen()
-}
+//#Preview {
+//    PickupScreen()
+//}
